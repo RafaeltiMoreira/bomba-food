e7f873a5c2d0e334627de8e186112387eb22ef8d
Rafael Moreira <rafaeltimoreirass@gmail.com>
2025-11-14 04:57:19 -0300

    feat: add scroll product details page

--- app/[slug]/menu/[productId]/components/product-details.tsx
+++ app/[slug]/menu/[productId]/components/product-details.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/app/_components/ui/button";
+import { ScrollArea } from "@/app/_components/ui/scroll-area";
 import { Prisma } from "@/app/generated/prisma/client";
 import { formatCurrency } from "@/helpers/format-currency";
 import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
@@ -51,10 +52,10 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
           </p>
         </div>
 
-        <h2 className="mt-1 text-xl font-semibold">{product.name}</h2>
+        <h2 className="mt-2 text-xl font-semibold">{product.name}</h2>
 
         {/* Preço e quantidade */}
-        <div className="flex items-center justify-between">
+        <div className="mt-3 mb-2 flex items-center justify-between">
           <h3 className="text-xl font-semibold">
             {formatCurrency(product.price)}
           </h3>
@@ -77,20 +78,24 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
           </div>
         </div>
 
-        {/* Descrição */}
-        <div className="mt-6 space-y-3">
-          <h4 className="font-semibold">Sobre</h4>
-          <p className="text-muted-foreground text-sm">{product.description}</p>
-        </div>
-
-        {/* Ingredientes */}
-        <div className="mt-6 space-y-3">
-          <div className="flex items-center gap-1">
-            <ChefHatIcon size={18} />
-            <h4 className="font-semibold">Ingredientes</h4>
+        <ScrollArea className="h-full">
+          {/* Descrição */}
+          <div className="mt-6 space-y-3">
+            <h4 className="font-semibold">Sobre</h4>
+            <p className="text-muted-foreground text-sm">{product.description}</p>
           </div>
-          <p className="text-muted-foreground text-sm">{product.description}</p>
-        </div>
+
+          {/* Ingredientes */}
+          <div className="mt-6 space-y-3">
+            <div className="flex items-center gap-1">
+              <ChefHatIcon size={18} />
+              <h4 className="font-semibold">Ingredientes</h4>
+            </div>
+            <ul className="list-disc px-5 text-sm text-muted-foreground">
+              {product.ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)}
+            </ul>
+          </div>
+        </ScrollArea>
       </div>
 
       {/* Adicionar ao carrinho */}
